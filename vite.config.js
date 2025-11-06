9e80dd07d23615e22f9f4d268988ce3db5c6afa9
Zino <zinoakpoghelie17@gmail.com>
2025-11-06 10:59:13 +0100

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "my-react-app/",
+  base: "/",
   plugins: [react()],
   build: {
     rollupOptions: {
